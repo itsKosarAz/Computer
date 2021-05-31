e86672ec94ef716c03333152c0818641c2327245
itsKosarAz <57855107+itsKosarAz@users.noreply.github.com>
2021-05-31 13:48:56 +0300

    Create Telecommunications.java

---
+++ SE421/DesignPatterns/DependencyInjection/Telecommunications.java
@@ -0,0 +1,15 @@
+
+public class Telecommunications implements Service {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doService() {
+		// TODO Auto-generated method stub
+		System.out.println("Did telecommunications service");
+	}
+
+}
