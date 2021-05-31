aadaecdaa1247f2f3cef2450dfdab791e213fec8
itsKosarAz <57855107+itsKosarAz@users.noreply.github.com>
2021-05-31 13:46:16 +0300

    Create NegativeNumbers.java

---
+++ SE421/DesignPatterns/ChainOfResponsibility/NegativeNumbers.java
@@ -0,0 +1,28 @@
+
+public class NegativeNumbers implements Numbers {
+	private Numbers next;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void processNumbers(int num) {
+		// TODO Auto-generated method stub
+		if (num < 0) {
+			System.out.println("The number is negative");
+		} else {
+			if (next != null) {
+				next.processNumbers(num);
+			}
+		}
+	}
+
+	@Override
+	public void setNumbers(Numbers next) {
+		// TODO Auto-generated method stub
+		this.next = next;
+	}
+
+}
