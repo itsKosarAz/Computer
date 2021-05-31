f408b1709f98e41a8eff6268c9a015f3906df0ec
itsKosarAz <57855107+itsKosarAz@users.noreply.github.com>
2021-05-31 13:47:04 +0300

    Create Zero.java

---
+++ SE421/DesignPatterns/ChainOfResponsibility/Zero.java
@@ -0,0 +1,28 @@
+
+public class Zero implements Numbers {
+	private Numbers next;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void processNumbers(int num) {
+		// TODO Auto-generated method stub
+		if (num == 0) {
+			System.out.println("The number is 0");
+		} else {
+			if (next != null) {
+				next.processNumbers(num);
+			}
+		}
+	}
+
+	@Override
+	public void setNumbers(Numbers next) {
+		// TODO Auto-generated method stub
+		this.next = next;
+	}
+
+}
